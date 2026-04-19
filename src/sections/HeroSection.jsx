76a5e13ed6f089a4604d2081ea9b00e9354f06be
Asish-kumar-sahu <asishkumar811805@gmail.com>
2026-04-19 19:37:35 +0530

    backup

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -102,7 +102,7 @@ export default function HeroSection() {
       >
         <h1 className={animate ? "fadeUp" : ""}>
           Transform Your Body <br />
-          <span style={{ color: "#facc15" }}>MAKE THE PAYMENT OF YOUR WEBSITE </span>
+          <span style={{ color: "#facc15" }}> BUT AT FIRST MAKE THE PAYMENT OF YOUR WEBSITE </span>
         </h1>
 
         <p className={animate ? "fadeUp delay1" : ""}>
